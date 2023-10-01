e8ffe65f29bb3addf145774605dc0d77effeef42
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2023-10-01 12:32:02 +0700

    Add files via upload

---
+++ WEB JAVASCRIPT/javascript.js
@@ -0,0 +1,9 @@
+function tampil(){
+    //document.querySelector("h1").innerText = "belajar javascript";
+   // document.querySelector(".container").style.backgroundColor = "red";
+   // document.querySelector("#para").style.fontSize = "50px";
+
+   console.log( document.querySelectorAll("img"));
+
+   document,querySelectorAll("img")[1].style.visibility="visibel"
+}
